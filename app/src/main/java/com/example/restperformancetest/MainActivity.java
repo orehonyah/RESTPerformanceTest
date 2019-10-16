ccaacf919ae2b60f39c2cd227ebe16db6fc01cff
Jung Jinwook <jinwook1240@gmail.com>
2019-10-16 16:54:47 +0900

    Update MainActivity and RecordManager
    Made record button and jobs registration logic
    Future jobs
    New Issue #2 How Can I Start Service?

--- app/src/main/java/com/example/restperformancetest/MainActivity.java
+++ app/src/main/java/com/example/restperformancetest/MainActivity.java
@@ -2,11 +2,8 @@ package com.example.restperformancetest;
 
 import android.os.Bundle;
 
-import com.example.restperformancetest.functions.SpeakingRecorder;
+import com.example.restperformancetest.functions.RecordManager;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import android.view.View;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -14,6 +11,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -21,6 +19,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,7 +32,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        final SpeakingRecorder recorder= new SpeakingRecorder((FloatingActionButton)findViewById(R.id.fab));
+        final RecordManager recordManager= RecordManager.manager;//녹음 관련 관리해주는 클래스.
+        final FloatingActionButton fab= findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "recording started", Snackbar.LENGTH_SHORT)
+                        .setAction("Action", null).show();
+                for (Runnable i:recordManager.jobs()) {//해당 상태일 때 해야 하는 job들을 한다.
+                    i.run();
+                }
+                if(recordManager.isSuccessful()){//만약 모든 job들이 성공적으로 끝났다면, recordManager
+                    if(recordManager.isRecording()){
+                        recordManager.setRecordingStateOff();
+                        fab.setImageResource(RecordManager.FLOATINGACTIONBUTTON_ON);
+                    }
+                    else{
+                        recordManager.setRecordingStateOn();
+                        fab.setImageResource(RecordManager.FLOATINGACTIONBUTTON_OFF);
+                    }
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Recording state change failed.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);//메뉴 서랍(옆에서 나오는 것)
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
